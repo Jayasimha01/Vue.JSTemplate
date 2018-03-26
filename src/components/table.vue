63d9af93526240498fd33ebecf1f8417a2bd231c
Vyshnavi Tanuku <vyshnavi.tanuku@ggktech.local>
2018-03-26 16:22:55 +0530

    initial template added

---
+++ src/components/table.vue
@@ -0,0 +1,53 @@
+<template>
+ <div>
+ <div class="col-xs-12 col-md-2 col-lg-2 "/>
+    <div class="dv_hometable">
+            <table class="table table-hover table-bordered table-fixed">
+            <thead>
+                <tr>
+                    <th style="width:35px">#</th>
+                    <th :style="{width:colinfo.width+'px'}" v-for="(colinfo) in columnInfo">
+                    {{ colinfo.columnName }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(objectItems,index) in items">
+                    <th>{{index + 1}}</th>
+                    <td v-for="(item, key, index) in objectItems">{{ item }}</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+    
+</div>
+</template>
+
+<script>
+export default {
+    props:['items','columnInfo'],
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style scoped>
+                
+.table-fixed {
+    border:1px solid;
+    table-layout: fixed;
+    width: 200px;
+    overflow:auto;
+
+}
+.table-fixed thead th {
+    background-color: #2a80af;
+    color: white;
+}
+.dv_hometable {
+    margin-top:20px;
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        Home view
+    </div>
+</template>
+
+<script>
+    import vtable from '../components/table.vue'
+    export default {
+        components: {
+            vtable
+        },
+        data() {
+            return {
+                items: [{
+                    link: 'https://vue-loader.vuejs.org/en/',
+                    productivity: '70%',
+                    suggestedby: 'abc',
+                    overallExperiance: 2,
+                    relevantExperience: 1
+                }, {
+                    link: 'https://vue-loader.vuejs.org/en/',
+                    productivity: '70%',
+                    suggestedby: 'xyz',
+                    overallExperiance: 3,
+                    relevantExperience: 3
+                }],
+                columnwidth: 150,
+                columnInfo: [{
+                    columnName: 'Link',
+                    width: 250, 
+                    type: 'link'
+                }, {
+                    columnName: 'Productivity',
+                    width: 120,
+                    type: 'normal'
+                }, {
+                    columnName: 'Suggested By',
+                    width: 120,
+                    type: 'normal'
+                },
+                {
+                    columnName: 'Over all Experience(yr)',
+                    width: 120,
+                    type: 'normal'
+                },{
+                    columnName: 'Relevant Experience(yr)',
+                    width: 120,
+                    type: 'normal'
+                }]
+            }
+        }
+    }
+</script>
+<style>
+    #main {
+        margin: 60px;
+    }
+</style>
